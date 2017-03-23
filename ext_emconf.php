9fb474e2ef14a0ee7ecf05112ad0e8eee2326022
Manuel Selbach <manuel_selbach@yahoo.de>
2017-03-23 18:03:47 +0100

    Update to new testing framework, raise version and dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => '(Ukrainian) language pack for the Static Info Tables providing localized names for countries, 
                        currencies and so on.',
     'category' => 'misc',
-    'version' => '6.3.3',
+    'version' => '6.4.0',
     'state' => 'stable',
     'uploadfolder' => true,
     'createDirs' => '',
@@ -17,9 +17,9 @@ $EM_CONF[$_EXTKEY] = [
         [
             'depends' =>
                 [
-                    'typo3' => '6.2.0-7.6.99',
-                    'php' => '5.5.0-0.0.0',
-                    'static_info_tables' => '6.3.0-6.3.99',
+                    'typo3' => '7.6.0-8.99.99',
+                    'php' => '5.6.0-0.0.0',
+                    'static_info_tables' => '6.4.0-6.4.99',
                 ],
             'conflicts' =>
                 [
